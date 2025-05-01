7647939b7c1af310a5511302aab2ca9ceca4fc31
fenx <alaa224101268@nmu.edu.eg>
2025-05-01 21:10:21 +0300

    Files committed

--- BankProject/obj/Debug/net8.0-windows/BankProject.AssemblyInfo.cs
+++ BankProject/obj/Debug/net8.0-windows/BankProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c2e3542f9fa5a57296c33a03e7b6b6134237bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2211a695cd09634c32632f10da0436d50a215424")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
