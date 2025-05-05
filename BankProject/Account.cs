922c783cb84cf4850efe495c02073137d842a22c
Nour Elden Mahmod <mnouralden1@gmail.com>
2025-05-05 20:58:40 +0300

    Delete Person class , Edit client class , Add two classes : Islamic saving account and Checking account , and inherit them from the Account class

--- BankProject/Account.cs
+++ BankProject/Account.cs
@@ -4,53 +4,48 @@ using System.Collections.Generic;
 
 namespace BankProject
 {
-    public class Account
-    {
-        public decimal balance { get; private set; }
-        private List<string> history = new List<string>();
-        private Client owner;
-
+    abstract public class Account
+    { 
+        protected Client owner;
+        protected decimal Balance;
+        protected List<string> history = new List<string>();
         public Account(Client owner)
         {
             this.owner = owner;
-            balance = 0;
+            Balance = 0;
         }
-
         public bool Deposit(decimal amount)
         {
             if (amount <= 0) return false;
 
-            balance += amount;
+            Balance += amount;
             history.Add($"Deposited: {amount} EGP on {DateTime.Now}");
             return true;
         }
-
         public bool Withdraw(decimal amount)
         {
-            if (amount <= 0 || amount > balance) return false;
+            if (amount <= 0 || amount > Balance) return false;
 
-            balance -= amount;
+            Balance -= amount;
             history.Add($"Withdrew: {amount} EGP on {DateTime.Now}");
             return true;
         }
-
         public bool Transfer(Client receiver, decimal amount)
         {
-            if (receiver == null || amount <= 0 || amount > balance) return false;
+            if (receiver == null || amount <= 0 || amount > Balance) return false;
 
-            balance -= amount;
+            Balance -= amount;
             history.Add($"Transferred: {amount} EGP to {receiver.Name} on {DateTime.Now}");
 
             receiver.ClientAccount.ReceiveTransfer(owner.Username, amount);
             return true;
         }
 
-        private void ReceiveTransfer(string senderUsername, decimal amount)
+        protected void ReceiveTransfer(string senderUsername, decimal amount)
         {
-            balance += amount;
+            Balance += amount;
             history.Add($"Received: {amount} EGP from {senderUsername} on {DateTime.Now}");
         }
-
         public List<string> GetHistory()
         {
             return new List<string>(history);
